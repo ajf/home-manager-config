cca440cb3f89ccdc6bf71e57091a61bf87b8f412
Andrew Forgue <andrew@forgue.io>
2026-08-31 20:24:13 -0700

    Revert "linux: add Kasts (podcast player)"
    This reverts commit 14de61a54b6493a35e4d8727d5f10b274c4104bf.

--- modules/linux.nix
+++ modules/linux.nix
@@ -74,7 +74,6 @@ lib.mkMerge [
       dms-shell # DankMaterialShell (`dms` CLI + quickshell config)
       kdePackages.dolphin # GUI file manager
       cider-2' # Apple Music client; wrapped with libpulse (see above)
-      kdePackages.kasts # podcast player
       obsidian
       slack
       teams-for-linux' # official Linux client is discontinued; Electron wrapper
